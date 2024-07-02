cde13bf7a2f7d506f2ce25af0eb0ce9812532fe7
banset <mohitbasnet80@gmail.com>
2024-07-02 22:29:57 +0545

    update settings

--- pokemon_project/settings.py
+++ pokemon_project/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pokemon_app'
+    'pokemon_app',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'pokemon_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
